325167ef9b801a3995ec359c3b9ab715992ca2f4
thiareecart <thiare.carvacho.cornejo@alumnos.cmch.maristas.cl>
2024-10-09 13:27:55 -0300

    xs

--- Ejercicio2.sh
+++ Ejercicio2.sh
@@ -1,15 +1,15 @@
-
 #!/bin/bash
 
-read -p "Ingrese un mensaje para el commit " Mensaje
+read -p "Ingresa el mensaje del commit: " mensaje
+
 git add .
 
-git commit -m "$Mensaje"
+git commit -m "$mensaje"
 
 remote=$(git remote)
-if [ -z "$remote" ]: then
-   echo "No hay repositorio remoto conectado"
-   exit 1
+if [ -z "$remote" ]; then
+  echo "No hay repositorio remoto configurado. Configura un repositorio remoto primero."
+  exit 1
 fi
 
 git push origin $(git branch --show-current)
