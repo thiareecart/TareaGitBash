4fd52bc63a4a81ad6fa8815d0d93c0d21f4923a0
thiareecart <thiare.carvacho.cornejo@alumnos.cmch.maristas.cl>
2024-10-09 13:26:49 -0300

    lol

--- Ejercicio2.sh
+++ Ejercicio2.sh
@@ -1,11 +1,13 @@
+
 #!/bin/bash
 
-read -p "Ingrese un mensaje para el commit " Mensaje 
+read -p "Ingrese un mensaje para el commit " Mensaje
 git add .
 
 git commit -m "$Mensaje"
 
 remote=$(git remote)
+
 if [ -z "$remote" ]: then
    echo "No hay repositorio remoto conectado"
    exit 1
@@ -13,4 +15,4 @@ fi
 
 git push origin $(git branch --show-current)
 
-echo "Cambios subidos a GitHub con éxito."   
+echo "Cambios subidos a GitHub con éxito."
